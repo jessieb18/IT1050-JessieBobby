f5e4fbecaf9b08621a8e48f4d6c403860da2069f
Jessie Bobby <jessieb1818@gmail.com>
2019-10-31 22:03:32 -0400

    Lab01 completed

---
+++ Lab01/Lab01/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab01
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("{0}\n(1)", "Welcome to ", "C# Programming!");
+			Console.WriteLine("Hello World from Jessie Bobby");
+			Console.Read();
+		}
+	}
+}
